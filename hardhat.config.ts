7efd552d40188b70bc55db059821279cc4f8e8fd
Alexander Gotovets <gotovets.mail@gmail.com>
2023-11-17 20:45:00 +0300

    Init commit

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+
+require('dotenv').config();
+
+const config = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      }
+    },
+  },
+  defaultNetwork: "localhost",
+  gasReporter: {
+    enabled: true,
+  },
+  networks: {
+    // hardhat: {
+    //   chainId: 1
+    // },
+    localhost: {
+      url: process.env.NETWORK_HOST_LOCALHOST,
+      accounts: [
+        process.env.OWNER_1_PK_LOCALHOST,
+        process.env.OWNER_2_PK_LOCALHOST,
+        process.env.OWNER_3_PK_LOCALHOST,
+        process.env.OWNER_4_PK_LOCALHOST,
+        process.env.OWNER_5_PK_LOCALHOST,
+      ],
+      chainId: 31337
+    },
+  },
+};
+
+export default config;
